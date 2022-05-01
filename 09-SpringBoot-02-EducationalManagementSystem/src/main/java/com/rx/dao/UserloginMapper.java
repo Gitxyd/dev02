4cda6cf328842696b4e8dd6ac728b67e0a31b4a1
xyd <3055267245@qq.com>
2022-05-01 15:50:17 +0800

    逆向工程

---
+++ 09-SpringBoot-02-EducationalManagementSystem/src/main/java/com/rx/dao/UserloginMapper.java
@@ -0,0 +1,30 @@
+package com.rx.dao;
+
+import com.rx.entity.Userlogin;
+import com.rx.entity.UserloginExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserloginMapper {
+    long countByExample(UserloginExample example);
+
+    int deleteByExample(UserloginExample example);
+
+    int deleteByPrimaryKey(Integer userid);
+
+    int insert(Userlogin record);
+
+    int insertSelective(Userlogin record);
+
+    List<Userlogin> selectByExample(UserloginExample example);
+
+    Userlogin selectByPrimaryKey(Integer userid);
+
+    int updateByExampleSelective(@Param("record") Userlogin record, @Param("example") UserloginExample example);
+
+    int updateByExample(@Param("record") Userlogin record, @Param("example") UserloginExample example);
+
+    int updateByPrimaryKeySelective(Userlogin record);
+
+    int updateByPrimaryKey(Userlogin record);
+}
